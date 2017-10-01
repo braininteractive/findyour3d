721fdab0796f589c96b840f32e684ddbb0fb76bc
hqpr <adubnyak@gmail.com>
2017-10-01 20:28:52 +0300

    Edit company

--- findyour3d/company/urls.py
+++ findyour3d/company/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^add/$', views.AddCompanyView.as_view(), name='add'),
     url(regex=r'^detail/(?P<pk>\w+)/$', view=views.CompanyDetailView.as_view(), name='detail'),
+    url(regex=r'^edit/(?P<pk>\w+)/$', view=views.EditCompanyView.as_view(), name='edit'),
 ]
--- findyour3d/company/views.py
+++ findyour3d/company/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseForbidden
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from .models import Company
 from .forms import AddCompanyForm
@@ -38,8 +38,8 @@ class AddCompanyView(LoginRequiredMixin, CreateView):
         kwargs = super(AddCompanyView, self).get_form_kwargs()
         kwargs.update(
             {'initial':
-                {'user': self.request.user}
-            }
+                 {'user': self.request.user}
+             }
         )
         return kwargs
 
@@ -55,4 +55,32 @@ class CompanyDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super(CompanyDetailView, self).get_context_data(**kwargs)
         context['company_pk'] = self.object
+        return context
+
+
+class EditCompanyView(LoginRequiredMixin, UpdateView):
+    model = Company
+    form_class = AddCompanyForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated():
+            if self.request.user.user_type == 2:
+                if not self.request.user.company_set.all():
+                    return redirect('company:add')
+        return super(EditCompanyView, self).dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('company:detail', kwargs={'pk': self.object.id})
+
+    def get_object(self, queryset=None):
+        return Company.objects.get(id=self.kwargs['pk'])
+
+    def get_initial(self):
+        initial_data = super(EditCompanyView, self).get_initial()
+        initial_data['user'] = self.request.user
+        return initial_data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
         return context
