257e11a685226081ecb697a50c7d9a6c863261ed
hqpr <adubnyak@gmail.com>
2018-08-23 11:09:11 +0300

    fix for confirmation emails and changing requesting logic

--- findyour3d/quote/views.py
+++ findyour3d/quote/views.py
@@ -24,48 +24,43 @@ def request_quote(request, pk):
             company_plan = company_owner.plan
             user = request.user
             if company_plan is not None:
-                if not QuoteRequest.objects.filter(company=company, user=request.user).exists():
-                    quote = QuoteRequest.objects.create(user=request.user, company=company)
+                quote = QuoteRequest.objects.create(user=request.user, company=company)
 
-                    # sending email to company
-                    send_templated_email('Your3d: New Request Received',
-                                         'email/customer_request.html',
-                                         {'user': user, 'company': company, 'quote': quote},
-                                         [company.email])
+                # sending email to company
+                send_templated_email('Your3d: New Request Received',
+                                     'email/customer_request.html',
+                                     {'user': user.customer_set.first(), 'company': company, 'quote': quote},
+                                     [company.email])
 
-                    # sending email to customer
-                    send_templated_email('Your3d: Request Confirmation',
-                                         'email/company_request.html',
-                                         {'user': user, 'company': company, 'quote': quote},
-                                         [user.email])
+                # sending email to customer
+                send_templated_email('Your3d: Request Confirmation',
+                                     'email/company_request.html',
+                                     {'user': user.customer_set.first(), 'company': company, 'quote': quote},
+                                     [user.email])
 
-                    if company_plan == 1:
-                        starter_charge(company_owner)
-                        company.quote_limit -= 1
-                        company.save()
-                        return custom_redirect('dashboard:company', quote='requested')
-                    else:
-                        return custom_redirect('dashboard:company', quote='requested')
-                else:
-                    logger.error('already requested')
-                    return redirect('dashboard:company')
-            elif company_owner.is_payment_active_or_free_coupon():
-                if not QuoteRequest.objects.filter(company=company, user=request.user).exists():
-                    quote = QuoteRequest.objects.create(user=request.user, company=company)
-
-                    # sending email to company
-                    send_templated_email('Your3d: New Request Received',
-                                         'email/customer_request.html',
-                                         {'user': user, 'company': company, 'quote': quote},
-                                         [company.email])
-
-                    # sending email to customer
-                    send_templated_email('Your3d: Request Confirmation',
-                                         'email/company_request.html',
-                                         {'user': user, 'company': company, 'quote': quote},
-                                         [user.email])
-
+                if company_plan == 1:
+                    starter_charge(company_owner)
+                    company.quote_limit -= 1
+                    company.save()
                     return custom_redirect('dashboard:company', quote='requested')
+                else:
+                    return custom_redirect('dashboard:company', quote='requested')
+            elif company_owner.is_payment_active_or_free_coupon():
+                quote = QuoteRequest.objects.create(user=request.user, company=company)
+
+                # sending email to company
+                send_templated_email('Your3d: New Request Received',
+                                     'email/customer_request.html',
+                                     {'user': user.customer_set.first(), 'company': company, 'quote': quote},
+                                     [company.email])
+
+                # sending email to customer
+                send_templated_email('Your3d: Request Confirmation',
+                                     'email/company_request.html',
+                                     {'user': user.customer_set.first(), 'company': company, 'quote': quote},
+                                     [user.email])
+
+                return custom_redirect('dashboard:company', quote='requested')
             else:
                 logger.error('company without plan')
                 return redirect('dashboard:company')
