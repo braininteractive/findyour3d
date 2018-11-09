c2e5d6fadacbb5e529e4e0154322434f868f4586
hqpr <adubnyak@gmail.com>
2018-11-09 16:36:43 +0200

    Making non-mandatory fields for company models

--- findyour3d/company/models.py
+++ findyour3d/company/models.py
@@ -47,7 +47,7 @@ class Company(models.Model):
     budget = MultiSelectField(choices=BUDGET_CHOICES, default=0)
     basic_material = models.IntegerField(choices=BASIC_MATERIAL_CHOICES, default=0)
     consideration = models.IntegerField(choices=CONSIDERATION_CHOICES, default=0)
-    printing_options = MultiSelectField(choices=PRINTING_OPTIONS_CHOICES)
+    printing_options = MultiSelectField(choices=PRINTING_OPTIONS_CHOICES, blank=True, null=True)
     material = MultiSelectField(choices=MATERIAL_CHOICES, blank=True, null=True)
     top_printing_processes = MultiSelectField(choices=PROCESS_CHOICES, max_choices=9, blank=True, null=True)
     description = models.TextField()
