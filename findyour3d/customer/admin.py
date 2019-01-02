59d3ff2bea9c31640e8ae9ccd81eaba1e50c86c4
hqpr <adubnyak@gmail.com>
2019-01-02 17:54:57 +0200

    Customer admin display

--- findyour3d/customer/admin.py
+++ findyour3d/customer/admin.py
@@ -3,6 +3,9 @@ from .models import Customer
 
 
 class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_budget_display', 'get_material',
+                    'get_process', 'is_expedited')
+    search_fields = ('user__username', 'user__email')
 
     fieldsets = [
         (None, {
@@ -14,6 +17,17 @@ class CustomerAdmin(admin.ModelAdmin):
                                          'description', 'cad_file')}),
         ('Shipping', {'fields': ('is_expedited', 'shipping')}),
     ]
+
+    def get_material(self, obj):
+        customer = Customer.objects.get(pk=obj.id)
+        return customer.get_material_display()
+
+    def get_process(self, obj):
+        customer = Customer.objects.get(pk=obj.id)
+        return customer.get_process_display()
+
+    get_material.short_description = 'material'
+    get_process.short_description = 'process'
 
 
 admin.site.register(Customer, CustomerAdmin)
